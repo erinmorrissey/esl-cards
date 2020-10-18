b0666acfe995e7b4926a43de91787648a90d6800
Erin Morrissey <erinmorrissey@gmail.com>
2020-10-17 18:48:34 -0700

    add infinite scroll with api data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import InfiniteScroll from "react-infinite-scroller";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const MAX_CARDS = 20;
+const URL = `https://api.elderscrollslegends.io/v1/cards?page=1&pageSize=${MAX_CARDS}`;
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cards: [],
+      hasMoreItems: true,
+      nextPage: null,
+    };
+    this.loadCards = this.loadCards.bind(this);
+  }
+
+  loadCards() {
+    const { cards, nextPage } = this.state;
+    const url = nextPage ? nextPage : URL;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data._links.next) {
+          this.setState({
+            cards: [...cards, ...data.cards],
+            nextPage: data._links.next,
+          });
+        } else {
+          this.setState({
+            cards: [...cards, ...data.cards],
+            nextPage: null,
+            hasMoreItems: false,
+          });
+        }
+      });
+  }
+
+  render() {
+    const cards = this.state.cards.map((card) => {
+      return <p>{card.name}</p>;
+    });
+
+    return (
+      <div className="App">
+        <InfiniteScroll
+          pageStart={0}
+          loadMore={this.loadCards}
+          hasMore={this.state.hasMoreItems}
+          loader={
+            <div className="loader" key={0}>
+              Loading ...
+            </div>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {cards}
+        </InfiniteScroll>
+      </div>
+    );
+  }
 }
 
 export default App;
